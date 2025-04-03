af76cb2393b6904d69ba367ae075475328a0caaa
Shashank <shakuvish07@gmail.com>
2025-04-03 16:15:32 +0530

    Update model.py

--- model.py
+++ model.py
@@ -55,10 +55,87 @@ class FeedForwardBlock(nn.Module):
         super().__init__()
         self.linear_1 = nn.Linear(d_model, d_ff)
         self.dropout = nn.Dropout(dropout)
-        self.Limear_2 = nn.Linear(d_ff, d_model)
+        self.linear_2inear_2 = nn.Linear(d_ff, d_model)
 
     def forward(self, x):
         return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
     
 
+class MultiheadAttentionBlock(nn.Module):
 
+    def __init__(self, d_model: int, h:int, dropout:float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h=h
+        assert d_model % h == 0 ,
+
+        self.d_k = d_model //h
+        self.w_q = nn.Linear(d_model,d_model)
+        self.w_k = nn.Linear(d_model,d_model)
+        self.w_v = nn.Linear(d_model,d_model)
+
+        self.w_o = nn.Linear(d_model,d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query,key,value,mask,dropout:nn.Dropout):
+        d_k=query.shape[-1]
+        attention_scores = (query @ key.transpose(-2,-1)) / math.sqrt(d_k)
+        if mask is not None:
+            attention_scores.masked_fill(mask ==0,-1e9)
+        attention_scores =attention_scores.softmax(dim=-1)
+        if dropout is not None:
+            attention_scores=dropout(attention_scores)
+        
+        return(attention_scores @ value), attention_scores
+
+
+    def forward(self, q,k,v,mask):
+        query =self.w_q(q)
+        key=self.w_k(k)
+        value=self.w_v(v)
+
+        query = query.view(query.shape[0],query.shape[1],self.h,self.d_k).transpose(1,2)
+        key= key.view(key.shape[0],key.shape[1], self.h,self.d_k).transpose(1,2)
+        value = value.view(value.shape[0],value.shape[1],self.h,self.d_k).transpose(1,2)
+
+        x,self.attention_scores=MultiheadAttentionBlock.attention(query,key,value,mask,self.dropout)
+
+        x=x.transpose(1,2).contiguous().view(x.shape[0],-1,self.h*self.d_k)
+        return self.w_o(x)
+
+class ResidualConnection(nn.Module):
+
+    def __init__ (self, dropout:float) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+
+    def forward(self,x,sublayer):
+        return x +self.dropout(sublayer(self.norm(x)))
+    
+
+class EncoderBlock(nn.Module):
+
+    def __init__(self,self_attention_block: MultiheadAttentionBlock, feed_forward_block: FeedForwardBlock,dropout:float) -> None:
+        super().__init__()
+        self.self_attention_block=self_attention_block
+        self.feed_forward_block=feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
+
+    def forward(self,x,src_mask):
+        x = self.residual_connections[0](x, lambda x:self.self_attention_block(x,x,x,src_mask))
+        x = self.residual_connections[1](x, self.feed_forward_block)
+        return x
+    
+class Encoder(nn.Module):
+    def __init__(self,layers:nn.ModuleList) -> None:
+         super().__init__()
+         self.layers = layers
+         self.norm = LayerNormalization()
+
+    def forward(self,x,mask):
+        for layer in self.layers:
+            x= layer(x, mask)
+
+        return self.norm(x)
