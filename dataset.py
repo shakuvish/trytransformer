94c84ee8231951f27cfd0df0fd97514c0611e6a7
Shashank <shakuvish07@gmail.com>
2025-04-06 22:52:05 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -29,4 +29,55 @@ class BilingualDataset(Dataset):
         enc_input_tokens= self.tokenizer_src.encode(src_text).ids
         dec_input_tokens=self.tokenizer_tgt.encode(tgt_text).ids
 
+        enc_num_padding_tokens=self.seq_len-len(enc_input_tokens)-2
+        dec_num_padding_tokens=self.seq_len-len(dec_input_tokens)-1
+
+        if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0:
+            raise ValueError('Sentence is too long')
         
+        encoder_input=torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(enc_input_tokens,dtype=torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token]*enc_num_padding_tokens,dtype=torch.int64)
+
+            ]
+
+        )
+
+        decoder_input=torch.cat(
+            [
+                self.sos_token,
+                torch.tensor([self.pad_token]*dec_num_padding_tokens,dtypr=torch.int64)
+
+
+                
+            ]
+
+
+        )
+
+        label=torch.cat(
+            [
+                torch.tensor(dec_input_tokens,dtype=torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token]*dec_num_padding_tokens,dtype=torch.int64)
+
+
+            ]
+        )
+
+        assert encoder_input.size(0) == self.seq_len
+        assert decoder_input.size(0) == self.seq_len
+        assert label.size(0) == self.seq_len
+
+        return{
+            "encoder_input":encoder_input,
+            "decoder_input":decoder_input,
+            "encoder_mask":(encoder_input !=self.pad_token).unsqueeze(0).unsqueeze(0).int(),
+            "decoder_mask": (decoder_input !=self.pad_token).int() & casual_mask(decoder_input.size(0))
+        }
+
+def casual_mask(size):
+    mask=torch.
