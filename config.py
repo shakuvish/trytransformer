4a179aa072b16047efb557e86cae8d02cb86f64d
Shashank <shakuvish07@gmail.com>
2025-04-10 15:44:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,8 @@ def get_config():
         "model_filename": "tmodel_",
         "preload":None,
         "tokenizer_file": "tokenizer_{0}.json",
-        "experiment_name": "runs/tmodel"
+        "experiment_name": "runs/tmodel",
+        "datasource": "opus_books"
     }
 
 def get_weights_file_path(config,epoch:str):
